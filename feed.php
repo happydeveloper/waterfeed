289dc3653992d5ccfb7f3882f3c6edb8c5671a3a
DURU <enfn2001@gmail.com>
2014-05-09 23:04:03 +0900

    fix sql

--- feed.php
+++ feed.php
@@ -2,6 +2,6 @@
 mysql_connect('localhost', 'root', '1111');
 mysql_select_db('farm');
 $flag = $_POST['flag'];
-$result = mysql_query('REPLACE INTO waterfeed (flag) VALUES('.$flag.')');
+$result = mysql_query('UPDATE waterfeed SET flag='.$flag);
 die(json_encode($flag));
 ?>
